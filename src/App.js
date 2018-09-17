0e7d4b092688a0ed2e10930d30747bb57fdd374a
MacBook-Air <macbook-air@PM-Air.local>
2018-09-17 15:44:40 +0800

    version 1.0.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import profilePhoto from './assets/profilePhoto.jpg'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='tc'>
-        <h1 className='f1 yellow bg-navy'> Daniel Naval </h1>
+     <div className='tc grow br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='Daniel Naval' src={profilePhoto} width='150'/>
+      <div>
+        <h2>Daniel Naval</h2>
+        <p>danielnvl5@gmail.com</p>
       </div>
+    </div>
     );
   }
 }
---
+++ src/components/businessCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const businessCard = ({businessCard}) => {
+	return (
+		<div className='tc grow br3 pa3 ma2 dib bw2 shadow-5'>
+		<img alt='Daniel Naval' src='../assets/profilePhoto.png'/>
+			<div>
+				<h2>Daniel Naval</h2>
+				<p>danielnvl5@gmail.com</p>
+			</div>
+		</div>
+		);
+}
